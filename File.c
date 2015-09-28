80f93878756da17bb87fba80b71715b9126e2242
amdaboss <positifoizo@gmail.com>
2015-09-28 18:18:12 +0200

    Update File.c

--- File.c
+++ File.c
@@ -6,12 +6,6 @@
 
 
 int main()
-{
-  printf("koukou maggle"); 
-<<<<<<< HEAD
-  printf("je suis ajubin, le pire barman du bar");
-=======
-  printf("fbtg");
->>>>>>> e4dd7a4de995df755a5342552eff7b9402c56a7e
+
   return 0;          
 }
